61130a5363d798dd63a532fb6f99914624250e4f
Peter McDonald <peter@Peters-Mac-Studio.local>
2025-03-16 14:31:51 +0000

    testing

--- Helpers/TerminalMode.cs
+++ Helpers/TerminalMode.cs
@@ -14,6 +14,6 @@ public class TerminalMode : IDisposable
     }
     public void Dispose()
     {
-        _sshClient.RunCommand(Mode.FromCommand);
+        _sshClient.RunCommand(Mode.FromCommand) ;
     }
 }
